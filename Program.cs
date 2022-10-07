2e1242a4bc55887e8f2f6135fa7843fcd8639362
Denis <epselent33@gmail.com>
2022-10-07 18:23:10 +0500

    выполнил первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да */
+
+int usernumber;
+Console.WriteLine("Введите пятизначное число");
+while (!int.TryParse(Console.ReadLine(), out usernumber))
+{
+	Console.WriteLine("Не верный ввод. Повторите");
+}
+bool palindromCheck = usernumber / 10000 == usernumber % 10 && (usernumber / 1000) % 10 == (usernumber % 100) / 10;
+if (palindromCheck)
+{
+	Console.WriteLine("Число палиндром");
+}
+else
+{
+	Console.WriteLine("Число не палиндром");
+}
