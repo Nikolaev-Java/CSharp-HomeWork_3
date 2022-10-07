2d8f5c2fbd963a29c0652a89114aa167bbbafa06
Denis <epselent33@gmail.com>
2022-10-07 18:40:38 +0500

    выполнил третье задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+int a;
+Console.WriteLine("Введите число");
+while (!int.TryParse(Console.ReadLine(), out a))
+{
+	Console.WriteLine("Не верный ввод. Повторите ввод");
+}
+for (int i = 1; i <= a; i++)
+{
+	Console.Write(Math.Pow(i, 3) + " ");
+}
